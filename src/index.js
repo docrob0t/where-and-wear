5107dab66e42ced9aabb1d5ed1ff471633713a48
James Greenfield <jg606@kent.ac.uk>
2020-11-09 00:38:58 +0000

    Make map fullscreen and begin adding current location weather card

--- src/index.js
+++ src/index.js
@@ -1,13 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+//Card styling constants
+const cardStyles = makeStyles({
+  root: {
+    width: 275,
+    height: 300,
+    margin: 35,
+    position: 'absolute',
+    bottom: 50,
+    borderRadius: 25,
+  },
+  moreDetails: {
+    position: 'absolute',
+    padding: '5%',
+    right: 0,
+    top: 0,
+    fontFamily: 'Calibri',
+  },
+  title: {
+    fontSize: 20,
+    top: '5%',
+    position: 'absolute',
+  },
+  secondTitle: {
+    fontSize: 20,
+    top: '40%',
+    position: 'absolute',
+  },
+  pos: {
+    marginTop: 12,
+    position: 'relative',
+  },
+});
+
+//CreateWeatherCard() function returns a weather card overlay
+export default function CreateWeatherCard() {
+  const classes = cardStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActions>
+        <Button size="small" className={classes.moreDetails}>More Details</Button>
+      </CardActions>
+      <CardContent>
+        <Typography className={classes.title} gutterBottom>Brighton</Typography>
+        <Typography className={classes.pos} color="textSecondary">25C / Sunny</Typography>
+        <Typography className={classes.secondTitle} gutterBottom>Clothing Suggestions</Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+//Map component
+class Map extends React.Component {
+  render() {
+    return <React.Fragment>
+      <div style={{ zIndex: 0 }}><iframe class="map-iframe"
+        src="https://embed.windy.com/embed2.html?lat=52.896&lon=-2.241&detailLat=51.914&detailLon=-1.346&width=1600&height=900&zoom=7&level=surface&overlay=wind&product=ecmwf&menu=&message=&marker=&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=default&metricTemp=default&radarRange=-1"
+        frameborder="0">
+      </iframe></div>
+
+      <div style={{ zIndex: 0 }}><CreateWeatherCard /></div>
+    </React.Fragment>
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <Map/>
+  </div>,
   document.getElementById('root')
 );
 
