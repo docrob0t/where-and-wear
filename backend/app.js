4109db4f61d75da719c6b92c21b13a9d62903cd5
James Greenfield <jg606@kent.ac.uk>
2021-01-27 14:17:18 +0000

    Minor change

--- backend/app.js
+++ backend/app.js
@@ -28,8 +28,8 @@ app.listen(port, () => console.log("Listening on port: " + port));
 // Return the current temperature and a 7 day forecast for a given set of lat/long co-ordinates
 app.post("/weatheratcoords", (req, res) => {
     // const requestURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + req.body.lat + '&lon=' + req.body.long + '&units=metric&exclude=hourly,minutely&appid=' + OPENWEATHERMAP_API_KEY;
-    // const requestURL = 'https://data.climacell.co/v4/timelines?location=' + req.body.lat + '%2C' + req.body.long + '&fields=temperature&fields=weatherCode&timesteps=1d&units=metric&apikey=' + CLIMACELL_API_KEY;
-    const requestURL = "https://data.climacell.co/v4/timelines?location=50.8586945%2C0.5598255&fields=temperature&fields=weatherCode&timesteps=1d&units=metric&apikey=sxYnAcSnTo09lDBvHEDbuVeDGnUPDoNr"
+    const requestURL = 'https://data.climacell.co/v4/timelines?location=' + req.body.lat + '%2C' + req.body.long + '&fields=temperature&fields=weatherCode&timesteps=1d&units=metric&apikey=' + CLIMACELL_API_KEY;
+    // const requestURL = "https://data.climacell.co/v4/timelines?location=50.8586945%2C0.5598255&fields=temperature&fields=weatherCode&timesteps=1d&units=metric&apikey=sxYnAcSnTo09lDBvHEDbuVeDGnUPDoNr"
 
     console.log('Weather request url: ' + requestURL);
 
@@ -43,6 +43,8 @@ app.post("/weatheratcoords", (req, res) => {
         .catch(function (error) {
             // TODO: Handle error
             console.log('############# ERROR !  ###############');
+            console.log('Req lat is: ' + req.lat);
+            console.log('Req long is: ' + req.long);
             console.log(error);
             console.log('############# END !  ###############');
         });
@@ -55,8 +57,8 @@ app.post("/locationfromcoords", (req, res) => {
         .then(function (response) {
             // handle success
             // TODO: Remove logging when no longer required
-            console.log('Request lat/long: ' + req.body.lat, req.body.long);
-            console.log('Response city is: ' + response.data.features[0].context[1].text);
+            // console.log('Request lat/long: ' + req.body.lat, req.body.long);
+            // console.log('Response city is: ' + response.data.features[0].context[1].text);
 
             // The mapbox API response for a reverse geocoding lookup is a bit convoluted
             // This seems to reference the city, but requires further testing to confirm 100%
