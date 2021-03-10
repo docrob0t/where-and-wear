83c48433bce68a8f88b37ba5ab5adb3716f84ab2
Marco Lam <cfl9@kent.ac.uk>
2021-03-10 12:35:58 +0000

    Add geolocate control and show user's location on map as a blue dot

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,12 +1,16 @@
 import "mapbox-gl/dist/mapbox-gl.css";
 
 import React, { useEffect, useRef, useState } from "react";
-import ReactMapGL, { FlyToInterpolator, WebMercatorViewport } from "react-map-gl";
+import ReactMapGL, {
+  FlyToInterpolator,
+  GeolocateControl,
+  WebMercatorViewport
+} from "react-map-gl";
 
 import { Box } from "@material-ui/core";
 import InputBox from "./InputBox/InputBox";
 import MenuButton from "./MenuButton";
-import Pins from "./Pins";
+import Pins from "./Pins/Pins";
 import WeatherCard from "./WeatherCard/WeatherCard";
 import axios from "../axios";
 import { easeQuadInOut } from "d3-ease";
@@ -14,6 +18,10 @@ import { easeQuadInOut } from "d3-ease";
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_API_KEY;
 
 function Map() {
+  const [userLocation, setUserLocation] = useState({
+    lat: undefined,
+    long: undefined
+  });
   const [startingPoint, setStartingPoint] = useState({
     lat: undefined,
     long: undefined
@@ -121,18 +129,26 @@ function Map() {
           search: city
         }
       })
-      .then((response) =>
+      .then((response) => {
+        setUserLocation({
+          lat: response.data.lat,
+          long: response.data.long
+        });
         setStartingPoint({
           lat: response.data.lat,
           long: response.data.long
-        })
-      );
+        });
+      });
   }
 
   function getCoordinates(position) {
     const { latitude, longitude } = position.coords;
+    setUserLocation({
+      ...userLocation,
+      lat: latitude,
+      long: longitude
+    });
     setStartingPoint({
-      ...startingPoint,
       lat: latitude,
       long: longitude
     });
@@ -165,6 +181,23 @@ function Map() {
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
       mapboxApiAccessToken={MAPBOX_TOKEN}
     >
+      <GeolocateControl
+        onViewportChange={(nextViewport) =>
+          setViewport({
+            ...viewport,
+            longitude: userLocation.long,
+            latitude: userLocation.lat,
+            zoom: 12,
+            transitionDuration: 3000,
+            transitionInterpolator: new FlyToInterpolator(),
+            transitionEasing: easeQuadInOut
+          })
+        }
+        style={{ position: "absolute", bottom: 30, right: 15 }}
+        positionOptions={{ enableHighAccuracy: true }}
+        showUserLocation={true}
+        auto
+      />
       {(function () {
         if (startingPoint.lat !== undefined && destination.lat !== undefined) {
           return (
@@ -176,10 +209,6 @@ function Map() {
               />
               <Pins isStart={false} lat={destination.lat} long={destination.long} />
             </Box>
-          );
-        } else if (startingPoint.lat !== undefined) {
-          return (
-            <Pins isStart={true} lat={startingPoint.lat} long={startingPoint.long} />
           );
         }
       })()}
